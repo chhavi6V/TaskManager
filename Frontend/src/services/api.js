390064211195358f2fbd8969f7ed25ef51207e42
chhavi6V <119997472+chhavi6V@users.noreply.github.com>
2024-11-28 21:55:27 +0530

    Update api.js

--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8050/api",
+  baseURL: "https://task-manager-tau-teal.vercel.app/api",
 });
 
 api.interceptors.request.use(
